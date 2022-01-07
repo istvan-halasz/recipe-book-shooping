e61e0512b7e59550aa4db114919716b86bc0afe1
Halasz Istvan <ihalasz@pentalog.com>
2022-01-07 18:42:47 +0200

    Submitting the Recipe Edit Form

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -15,6 +15,11 @@ export class RecipeListComponent implements OnInit {
   }
 
   ngOnInit(): void {
+    this.recipeService.recipesChanged.subscribe(
+      (newRecipes: Recipe[]) => {
+        this.recipes = newRecipes;
+      }
+    );
     this.recipes = this.recipeService.getRecipes();
   }
 
