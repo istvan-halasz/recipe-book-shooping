7245496c66930fc67a705eeda2b1f54422758fc7
istvan-halasz <haistvan@gmail.com>
2022-01-16 18:51:08 +0200

    use take

--- src/app/auth/auth.guard.ts
+++ src/app/auth/auth.guard.ts
@@ -1,7 +1,7 @@
 import { Injectable } from "@angular/core";
 import { ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot, UrlTree } from "@angular/router";
 import { Observable } from "rxjs";
-import { map, tap } from "rxjs/operators";
+import { map, take, tap } from "rxjs/operators";
 import { AuthService } from "./auth.service";
 
 @Injectable({ providedIn: 'root' })
@@ -9,7 +9,7 @@ export class AuthGard implements CanActivate {
   constructor(private authService: AuthService, private router: Router) { }
 
   canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): boolean | UrlTree | Observable<boolean | UrlTree> | Promise<boolean | UrlTree> {
-    return this.authService.user.pipe(map(user => {
+    return this.authService.user.pipe(take(1), map(user => {
       const isAuth = !!user;
       if (isAuth) {
         return true;
