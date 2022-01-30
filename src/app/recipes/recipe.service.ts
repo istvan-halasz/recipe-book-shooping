56ab1428975ec2f8e8e70a8ba0446a0ab47c0be7
istvan-halasz <haistvan@gmail.com>
2022-01-30 17:06:49 +0200

    add multiple ingredients action

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,8 +1,11 @@
 import { Injectable } from "@angular/core";
+import { Store } from "@ngrx/store";
 import { Subject } from "rxjs";
 import { Ingredient } from "../shared/ingredient.model";
 import { ShoppingListService } from "../shopping-list/shopping-list.service";
 import { Recipe } from "./recipe.model";
+import * as ShoppingListActions from "../shopping-list/store/shopping-list.actions";
+import { Action } from "rxjs/internal/scheduler/Action";
 
 @Injectable()
 export class RecipeService {
@@ -28,7 +31,7 @@ export class RecipeService {
 
   private recipes: Recipe[] = [];
 
-  constructor(private shoppingListService: ShoppingListService) { }
+  constructor(private shoppingListService: ShoppingListService, private store: Store<{ shoppingList: { ingredients: Ingredient[]; }; }>) { }
 
   setRecipes(recipes: Recipe[]) {
     this.recipes = recipes;
@@ -45,7 +48,8 @@ export class RecipeService {
   }
 
   addIngredientsToShoppingList(ingredients: Ingredient[]) {
-    this.shoppingListService.addIngredients(ingredients);
+    //this.shoppingListService.addIngredients(ingredients);
+    this.store.dispatch(new ShoppingListActions.AddIngredients(ingredients));
   }
 
   addRecipe(recipe: Recipe) {
