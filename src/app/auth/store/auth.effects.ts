9d5e7dddc817f94ad5365d87ebe842dc5db511cf
istvan-halasz <haistvan@gmail.com>
2022-02-13 13:02:19 +0200

    Adding Auto-Login with NgRx

--- src/app/auth/store/auth.effects.ts
+++ src/app/auth/store/auth.effects.ts
@@ -6,11 +6,14 @@ import { of } from "rxjs";
 import { catchError, map, switchMap, tap } from "rxjs/operators";
 import { environment } from "src/environments/environment";
 import { AuthResponseData } from "../auth.service";
+import { User } from "../user.model";
 
 import * as AuthActions from './auth.actions';
 
 const handleAuthentication = (expiresIn: number, email: string, userId: string, token: string) => {
   const expirationDate = new Date(new Date().getTime() + expiresIn * 1000);
+  const user = new User(email, userId, token, expirationDate);
+  localStorage.setItem('userData', JSON.stringify(user));
   return new AuthActions.AuthenticateSuccess({
     email: email,
     userId: userId,
@@ -82,6 +85,44 @@ export class AuthEffects {
     tap(() => {
       this.router.navigate(['/']);
     }));
+
+  @Effect({ dispatch: false })
+  authLogout = this.actions$.pipe(ofType(AuthActions.LOGOUT),
+    tap(() => {
+      localStorage.removeItem('userData');
+    }));
+
+  @Effect()
+  autoLogin = this.actions$.pipe(ofType(AuthActions.AUTO_LOGIN),
+    map(() => {
+      const userData: {
+        email: string,
+        id: string,
+        _token: string,
+        _tokenExpirationDate: string;
+      } = JSON.parse(localStorage.getItem('userData'));
+      if (!userData) {
+        return { type: 'DUMMY' };
+      }
+
+      const loadedUser = new User(userData.email, userData.id, userData._token, new Date(userData._tokenExpirationDate));
+
+      if (loadedUser.token) {
+        //this.user.next(loadedUser);
+        return new AuthActions.AuthenticateSuccess({
+          email: loadedUser.email,
+          userId: loadedUser.id,
+          token: loadedUser.token,
+          expirationDate: new Date(userData._tokenExpirationDate)
+        });
+        
+
+        return { type: 'DUMMY' };
+        //const expirationDuration = new Date(userData._tokenExpirationDate).getTime() - new Date().getTime();
+        //this.autoLogout(expirationDuration);
+      }
+    })
+  );
 
 
   constructor(private actions$: Actions, private httpClient: HttpClient, private router: Router) {
